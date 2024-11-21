3d641a787968d785b5df295edfcdbfb5767a583c
olawalesegun <olawalesegun0@gmail.com>
2024-11-22 02:26:09 +0300

    deployment of manaufacturerer verification

---
+++ src/data/dummyManufacturers.js
@@ -0,0 +1,25 @@
+const manufacturers = [
+    {
+        verified: true,
+        brandName: 'Rite Foods Ng Ltd',
+        registrationNumber: '123456789',
+        country: 'Nigeria',
+        
+    },
+    {
+        verified: true,
+        brandName: 'Matagbamole Global Industries',
+        registrationNumber: '987654321',
+        country: 'Nigeria',
+    },
+    {
+        verified: true,
+        brandName: 'DanGari Beverage Productions',
+        registrationNumber: '456123789',
+        country: 'Nigeria',
+        
+    },
+  ];
+  
+  module.exports = manufacturers;
+  
